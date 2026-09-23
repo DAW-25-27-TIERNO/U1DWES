c9a3095dcaaa72b07288c7f4cbc9939436ef0937
seteruiz <mruiz29@educa.madrid.org>
2026-09-23 12:18:19 +0200

    bucles array

--- arraysAdvanced.php
+++ arraysAdvanced.php
@@ -20,48 +20,65 @@ include "infoArrays/restaurants.php";
         echo $p[0]["address"];
         ?>
     </p>
-    <p>El número de camareros de Luigi es 
+    <p>El número de camareros de Luigi es
         <?= $p[1]['employees'][1] ?>
     </p>
     <p>
-        El número de bebidas de Carpaccio es 
-        <?php 
+        El número de bebidas de Carpaccio es
+        <?php
         echo $p[0]['quantity']['drinks'];
         ?>
     </p>
-    <p>El nombre de los 3 restaurantes obtenidos con un bucle es: 
+    <p>El nombre de los 3 restaurantes obtenidos con un bucle es:
         <?php
-            //Versión con for:
-            for ($i=0; $i < count($p); $i++) { 
-                echo $p[$i]["name"] . " ";
-            }
-            echo "<br>";
+        //Versión con for:
+        for ($i = 0; $i < count($p); $i++) {
+            echo $p[$i]["name"] . " ";
+        }
+        echo "<br>";
 
-            //Versión con foreach:
-            foreach ($p as $r) {
-                echo $r["name"] . " ";
-            }
+        //Versión con foreach:
+        foreach ($p as $r) {
+            echo $r["name"] . " ";
+        }
         ?>
-        <p>Los restaurantes son:</p>
-        <ul>
-            <?php
-            $x = 1;
-            foreach ($p as $r) {
-                //echo "<li>$x " . $r['name'] . "</li>";
-                echo "<li>$x {$r['name']} </li>";
-                $x++;
-            }
-        ?>
-        </ul>
-
-        <p>Los empleados de ambos restaurantes: </p>
+    <p>Los restaurantes son:</p>
+    <ul>
         <?php
-        /*Quiero que salga algo como esto (BUCLE):
+        $x = 1;
+        foreach ($p as $r) {
+            //echo "<li>$x " . $r['name'] . "</li>";
+            echo "<li>$x {$r['name']} </li>";
+            $x++;
+        }
+        ?>
+    </ul>
+
+    <p>Los empleados de ambos restaurantes: </p>
+    <?php
+    /*Quiero que salga algo como esto (BUCLE):
         Carpacio: 4, 7, 2
         Luigi: 2, 6, 1
         */
+    foreach ($p as $r) {
+        //Imprimo el nombre del restaurante
+        echo "{$r['name']}: ";
+        //Imprimo el array de números employees:
+        //echo $r["employees"];
+        //Necesito comprobar que existe $r['employees']
+        if (isset($r['employees'])) {
+            foreach ($r['employees'] as $e) {
+                echo "$e, ";
+            }
+        } else {
+            echo "No hay empleados";
+        }
+        echo "<br>";
+    }
 
-        ?>
+    /*Luego en una table de HTML */
+
+    ?>
     </p>
 
 </body>
